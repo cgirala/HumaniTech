52bf4920bf21ed88afa24ad37cf19c227d880478
Christian Girala <christiangirala@gmail.com>
2016-04-02 20:38:27 -0400

    service should call mp3 file

--- app/src/main/java/com/example/meganyang/humanitech/RingtoneService.java
+++ app/src/main/java/com/example/meganyang/humanitech/RingtoneService.java
@@ -2,17 +2,18 @@ package com.example.meganyang.humanitech;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
-
-import java.security.Provider;
+import android.widget.Toast;
 
 /**
  * Created by Christian Girala on 4/2/2016.
  */
 public class RingtoneService extends Service {
 
+    MediaPlayer song;
 
     @Nullable
     @Override
@@ -20,8 +21,20 @@ public class RingtoneService extends Service {
         return null;
     }
 
-    public int onStart(Intent intent) {
-        Log.i("Within the RingTone", "Service is true");
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("LocalService", "Received start id " + startId + ": " + intent);
+
+        song = MediaPlayer.create(this, R.raw.dancin_krono_remix);
+        song.start();
+
         return START_NOT_STICKY;
     }
+
+    @Override
+    public void onDestroy() {
+        // Tell the user we stopped.
+        Toast.makeText(this, "onDestroy called", Toast.LENGTH_SHORT).show();
+    }
+
 }
